f7c72050a4ec8534968bd9544ded443ce6e383f2
Zhanna Smirnova <>
2017-12-01 10:50:41 +0300

    add screenshot method

--- src/test/java/TrivagoTest.java
+++ src/test/java/TrivagoTest.java
@@ -1,8 +1,12 @@
+import com.codeborne.selenide.Screenshots;
+import com.google.common.io.Files;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.Attachment;
 import ru.yandex.qatools.allure.annotations.Title;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -10,7 +14,6 @@ import java.util.Date;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
-import static com.codeborne.selenide.ex.ErrorMessages.screenshot;
 
 /**
  * Created by zsmirnova on 11/22/17.
@@ -35,7 +38,7 @@ public class TrivagoTest {
 
     @Test
     @Title("Check hotel is displayed with free wi-fi filter")
-    public void freeWiFiDisplayHotel(){
+    public void displayHotelWithWiFiTest(){
         trivagoResultPage
                 .openTopFilters()
                 .setTopFilter("Free WiFi")
@@ -44,7 +47,7 @@ public class TrivagoTest {
 
     @Test
     @Title("Check hotel is not displayed with free wi-fi filter")
-    public void freeWiFiNotDisplayHotel(){
+    public void notDisplayHotelWithWiFiTest(){
         trivagoResultPage
                 .openTopFilters()
                 .setTopFilter("Free WiFi")
@@ -53,7 +56,7 @@ public class TrivagoTest {
 
     @Test
     @Title("Check hotel is displayed with Spa filter")
-    public void SpaDisplayHotel(){
+    public void displayHotelWithSpaTest(){
         trivagoResultPage
                 .openExtraFilters()
                 .setExtraFilter("Hotel facilities", "Wellness Center / Spa")
@@ -62,7 +65,7 @@ public class TrivagoTest {
 
     @Test
     @Title("Check hotel is not displayed with Spa filter")
-    public void SpaNotDisplayHotel(){
+    public void notDisplayHotelWithSpaTest(){
         trivagoResultPage
                 .openExtraFilters()
                 .setExtraFilter("Hotel facilities", "Wellness Center / Spa")
@@ -73,6 +76,12 @@ public class TrivagoTest {
     public void tearDown() throws IOException {
         screenshot();
         clearBrowserCache();
+    }
+
+    @Attachment(type = "image/png")
+    public byte[] screenshot() throws IOException {
+        File screenshot = Screenshots.getLastScreenshot();
+        return Files.toByteArray(screenshot);
     }
 
 }
